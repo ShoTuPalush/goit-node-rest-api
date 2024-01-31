2489543114c10d333b738346247d6f1f0eea7644
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-01-31 19:24:12 +0200

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,47 +2,50 @@ import HttpError from "../helpers/HttpError.js";
 import { Contact } from "../models/contacts.js";
 
 export const getAllContacts = async (req, res) => {
-  const result = await Contact.find();
-  res.json(result);
+    const result = await Contact.find();
+    res.json(result);
 };
 
 export const getOneContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findById(id).catch(() => {
-    throw HttpError(404);
-  });
-  res.json(result);
+    const { id } = req.params;
+    const result = await Contact.findById(id).catch(() => {
+        throw HttpError(404);
+    });
+    res.json(result);
 };
 
 export const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndDelete(id).catch(() => {
-    throw HttpError(404);
-  });
-  res.json(result);
+    const { id } = req.params;
+    const result = await Contact.findByIdAndDelete(id).catch(() => {
+        throw HttpError(404);
+    });
+    res.json(result);
 };
 
 export const createContact = async (req, res) => {
-  const result = await Contact.create(req.body);
-  res.status(201).json(result);
+    const result = await Contact.create(req.body);
+    res.status(201).json(result);
 };
 
 export const updateContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, {
-    new: true,
-  }).catch(() => {
-    throw HttpError(404);
-  });
-  res.json(result);
+    const { id } = req.params;
+    if (Object.keys(req.body).length === 0) {
+        throw HttpError(400, "Body must have at least one field");
+    }
+    const result = await Contact.findByIdAndUpdate(id, req.body, {
+        new: true,
+    }).catch(() => {
+        throw HttpError(404);
+    });
+    res.json(result);
 };
 
 export const updateStatusContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, {
-    new: true,
-  }).catch(() => {
-    throw HttpError(404);
-  });
-  res.json(result);
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate(id, req.body, {
+        new: true,
+    }).catch(() => {
+        throw HttpError(404);
+    });
+    res.json(result);
 };
