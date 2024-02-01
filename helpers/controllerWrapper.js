c200e07a828f9c59b0092d5b86c161bf1d12fac9
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-02-01 14:04:58 +0200

    debug

--- helpers/controllerWrapper.js
+++ helpers/controllerWrapper.js
@@ -1,12 +1,12 @@
-const controllerWrapper = controller => {
-    const func = async (req, res, next) => {
-        try {
-            await controller(req,res,next)
-        } catch (error) {
-            next(error)
-        }
+const controllerWrapper = (controller) => {
+  const func = async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
     }
-    return func
-}
+  };
+  return func;
+};
 
-export default controllerWrapper  
+export default controllerWrapper;
