1811894d653f359e15a40d39189e55942757e2b6
ShoTuPalush <131718941+ShoTuPalush@users.noreply.github.com>
2024-02-10 12:11:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use('/avatars', express.static(__dirname + '/public'));
+app.use('/users/avatars', express.static(__dirname + '/public'));
 
 app.use('/api/contacts', contactsRouter);
-app.use('/api/users', usersRouter);
+app.use('/users', usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Route not found' });
